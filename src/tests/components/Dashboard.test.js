bf0d2bfeece0e4a15cb2dc3d23e18c49b72ef5c7
Thegaijin <ssemagandavictor@gmail.com>
2018-03-21 12:53:16 +0300

    [ch #156145809] Edit Dashboard tests

--- src/tests/components/Dashboard.test.js
+++ src/tests/components/Dashboard.test.js
@@ -1,49 +1,86 @@
-import Enzyme, { render, mount } from 'enzyme';
+// import Enzyme, { render, mount } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// import ReactDOM from 'react-dom';
+// import configureMockStore from 'redux-mock-store';
+// import { MemoryRouter } from 'react-router-dom';
+// import thunk from 'redux-thunk';
+// import { Provider } from 'react-redux';
+// import React from 'react';
+
+// import { Dashboard } from '../../_components/Dashboard';
+
+// Enzyme.configure({ adapter: new Adapter() });
+
+// describe('<Dashboard />', () => {
+//   const store = configureMockStore([thunk])({
+//     authentication: {},
+//     categoryReducer: {
+//       categories: [],
+//     },
+//     recipeReducer: {},
+//   });
+
+//   const props = {};
+//   const preventDefault = jest.fn();
+//   const component = mount(
+//     <Provider store={store}>
+//       <MemoryRouter>
+//         <Dashboard {...props} />
+//       </MemoryRouter>
+//     </Provider>,
+//   );
+//   it('should render itself without crashing', () => {
+//     const { enzymeWrapper } = render(
+//       <Provider store={store}>
+//         <MemoryRouter>
+//           <Dashboard {...props} />
+//         </MemoryRouter>
+//       </Provider>,
+//     );
+//   });
+//   it('should render form', () => {
+//     expect(component.find('form').length).toBe(1);
+//     expect(component.find('form').simulate('submit', { preventDefault }));
+//     expect(preventDefault).toBeCalled();
+//   });
+//   it('should render form', () => {
+//     expect(component).toHaveLength(1);
+//   });
+// });
+
+import React from 'react';
+import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import configureMockStore from 'redux-mock-store';
-import { MemoryRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import React from 'react';
 
-import { Dashboard } from '../../_components/Dashboard';
+import Dashboard from '../../_components/Dashboard';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Dashboard />', () => {
   const store = configureMockStore([thunk])({
-    authentication: {},
-    categoryReducer: {
-      categories: [],
+    user: {
+      'Access Token': '12345trewdfgw23456wedfvcdsq234543wddsqw345',
     },
-    recipeReducer: {},
   });
 
-  const props = {};
-  const preventDefault = jest.fn();
-  const component = mount(
-    <Provider store={store}>
-      <MemoryRouter>
-        <Dashboard {...props} />
-      </MemoryRouter>
-    </Provider>,
-  );
+  const props = {
+    categories: [],
+    recipes: [],
+    categoryPages: 1,
+    recipePages: 1,
+    currentCategoryPage: 1,
+    currentRecipePage: 1,
+    categoryId: 1,
+  };
   it('should render itself without crashing', () => {
-    const { enzymeWrapper } = render(
+    mount(
       <Provider store={store}>
-        <MemoryRouter>
-          <Dashboard {...props} />
-        </MemoryRouter>
+        <Dashboard {...props} />
       </Provider>,
     );
-  });
-  it('should render form', () => {
-    expect(component.find('form').length).toBe(1);
-    expect(component.find('form').simulate('submit', { preventDefault }));
-    expect(preventDefault).toBeCalled();
-  });
-  it('should render form', () => {
-    expect(component).toHaveLength(1);
   });
 });
